d6a1e99a4ff1f094f11b26b3b97b713760e68621
Zineng Tang <82334223+zinengtang@users.noreply.github.com>
2022-12-13 23:20:38 -0500

    Delete i-Code-Doc/core/datasets directory

--- i-Code-Doc/core/datasets/__init__.py
+++
@@ -1,7 +0,0 @@
-from .visual_rvl_cdip import RVLCdipImageDataset
-from .publaynet import Publaynet
-from .docbank import Docbank
-from .websrc import Websrc
-from .visualmrc import VisualMRC
-from .duebenchmark import Duebenchmark
-from .iitcdip import IITCDIPDataset
