cbabf808b9af0a0f860a8825982eb89c3d701c93
Zineng Tang <82334223+zinengtang@users.noreply.github.com>
2023-02-03 14:40:17 -0500

    Update udop_config.py

--- i-Code-Doc/core/models/udop_config.py
+++ i-Code-Doc/core/models/udop_config.py
@@ -2,11 +2,11 @@ from typing import Any, Dict, Optional, Sequence
 
 from transformers.models.t5.configuration_t5 import T5Config
 
-Udop_PRETRAINED_CONFIG_ARCHIVE_MAP = {}
+UDOP_PRETRAINED_CONFIG_ARCHIVE_MAP = {}
 
 
 class UdopConfig(T5Config):
-    pretrained_config_archive_map = Udop_PRETRAINED_CONFIG_ARCHIVE_MAP
+    pretrained_config_archive_map = UDOP_PRETRAINED_CONFIG_ARCHIVE_MAP
 
     def __init__(self, 
                  max_2d_position_embeddings=1024,
@@ -15,6 +15,9 @@ class UdopConfig(T5Config):
                  mae_checkpoint = 'mae-models/mae_pretrain_vit_large_full.pth',
                  image_size: int = 224,
                  relative_bias_args: Optional[Sequence[Dict[str, Any]]] = [{"type":"1d"},{"type":"horizontal"},{"type":"vertical"}],
+                 data_dir: str = '.',
+                 truncate_decoder_after_layer: Optional[int] = None,
+                 truncate_encoder_after_layer: Optional[int] = None,
                  **kwargs):
         super().__init__(**kwargs)
         
@@ -25,3 +28,8 @@ class UdopConfig(T5Config):
 
         self.relative_bias_args = [] if relative_bias_args is None else relative_bias_args
         self.image_size = image_size
+        
+        self.data_dir = data_dir
+        
+        self.truncate_decoder_after_layer = truncate_decoder_after_layer
+        self.truncate_encoder_after_layer = truncate_encoder_after_layer
