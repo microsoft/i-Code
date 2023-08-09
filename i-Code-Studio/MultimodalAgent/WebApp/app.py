aeb04ab63b61bab23bac540e6c08e91917933aab
m-khad <114986625+m-khad@users.noreply.github.com>
2023-08-08 17:07:31 -0700

    Add files via upload

---
+++ i-Code-Studio/MultimodalAgent/WebApp/app.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 IBM Corp. All Rights Reserved.
+
+# Licensed under the Apache License, Version 2.0 (the “License”)
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an “AS IS” BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import os
+import re
+from dotenv import load_dotenv
+from flask import Flask, Response
+from flask import jsonify
+from flask import request, redirect
+from flask_socketio import SocketIO
+from flask_cors import CORS
+from tools.aml_asr import SpeechToText
+from tools.aml_tts import TextToSpeech
+from tools.openai_gpt3 import GPT3Generator
+from tools.aml_cv_wrapper import ComputerVisionWrapper
+import openai
+app = Flask(__name__)
+socketio = SocketIO(app)
+CORS(app)
+
+openai.api_key = "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"
+# Set up the model (more models, visit https://beta.openai.com/playground)
+model_engine = "text-davinci-003"
+
+# Define a function that sends a message to ChatGPT
+def chat_query(prompt):
+    completions = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=32,
+        n=1,
+        temperature=0.5,
+    )
+
+    message = completions.choices[0].text
+    return message
+
+gpt3 = GPT3Generator()
+asr = SpeechToText()
+tts_service = TextToSpeech()
+cv = ComputerVisionWrapper()
+
+
+# Redirect http to https on CloudFoundry
+@app.before_request
+def before_request():
+    fwd = request.headers.get('x-forwarded-proto')
+
+    # Not on Cloud Foundry
+    if fwd is None:
+        return None
+    # On Cloud Foundry and is https
+    elif fwd == "https":
+        return None
+    # On Cloud Foundry and is http, then redirect
+    elif fwd == "http":
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
+
+
+@app.route('/')
+def Welcome():
+    return app.send_static_file('index.html')
+
+
+@app.route('/api/conversation', methods=['POST', 'GET'])
+def getConvResponse():
+    query = request.form.get('convText')
+    convContext = request.form.get('context', "{}")
+    context = json.loads(convContext)
+
+    start = "Hello, I am a multimodal agent created by Azure Cognitive Services. How can I help you today?"
+    def format_gpt3_prompt(context, query, vis_input):
+        # prompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n"
+        
+        prompt = """The following is a conversation with an AI assistant. The assistant can interact with the human by seeing.\n
+                    AI: Hello
+                    Human: Hey, nice to meet you. (seeing: A man wearing glasses. Amazon. bagpack)
+                    AI: Hi. Are you working in Amazon?
+                    Human: Yes, how do you know?
+                    AI: I guess it from your bagpack.
+                    Human: Can you guess something from me? (seeing A man playing piano. piano)
+                    AI: You must like music.
+                    Human: Yes. How did you find it?
+                    AI: Because you are playing a piano.
+                    Human: Thank you. Have a nice day.
+                    AI: You're welcome. Have a nive day too.
+                    """
+        if not context:
+            return prompt + f"AI:{start}\nHuman: {query}"
+        else:
+            return prompt + f"{context['history']}\nHuman: {query} (seeing: {vis_input['caption']} {vis_input['objects']})"
+
+    vis_output = cv.get_vision_signals()
+    #vis_output1 = {"caption": "a person wearing glasses.", "objects": "Microsoft"}
+    #vis_output2 = {"caption": "a person drinking from a can.", "objects": "coca cola"}
+    print(f"query: {query}; vision: {vis_output}")
+        
+    if not query and query != "":
+        # If camera available, start the conversation with the caption
+        
+        history = f"AI: {start}"
+        response = start
+    else:
+        if query == "Can you tell me?":
+            response = "I didn't catch the previous sentence. Could you say that again, please?"
+        else:
+            # if "why" in query.lower() or "drink" in query or "drinks" in query:
+            #     vis_output = vis_output2
+            # else:
+            #     vis_output = vis_output1
+            prompt = format_gpt3_prompt(context, query, vis_output)
+            print('*******************', prompt, '*****************')
+            response = gpt3(prompt)
+            # response = chat_query(prompt)
+        if response.startswith("AI: "):
+            response = response[4:]
+
+        history = f"\nHuman: {query}\nAI: {response}"
+
+    if not context:
+        context['history'] = history 
+    else:
+        context['history'] += history
+
+    if response.startswith("AI:"):
+        response = response[3:]
+    responseDetails = {'responseText': response,
+                       'context': context}
+
+    return jsonify(results=responseDetails)
+
+
+@app.route('/api/text-to-speech', methods=['POST'])
+def getSpeechFromText():
+    input_text = request.form.get('text')
+
+    def generate():
+        if input_text:
+            data = tts_service(input_text)
+        else:
+            print("Empty response")
+            data = "I have no response to that."
+
+        yield data
+
+    return Response(response=generate(), mimetype="audio/x-wav")
+
+
+@app.route('/api/speech-to-text', methods=['POST', 'GET'])
+def getTextFromSpeech():
+    text_output, reason = asr()
+    print("##################", reason)
+    # append CV caption
+    #cv_caption = cv.getCaption()
+    #if cv_caption:
+    #    text_output += " (Seeing) " + cv_caption
+
+    return jsonify(results={'asr': text_output})
+
+
+@app.route('/api/computer-vision', methods=['POST', 'GET'])
+def getCVCaptionText():
+    text_output = cv.getCaption()
+
+    return jsonify(results={'cv': text_output})
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(cv.gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+port = os.environ.get("PORT") or os.environ.get("VCAP_APP_PORT") or 8080
+if __name__ == "__main__":
+    load_dotenv()
+
+    socketio.run(app, host='0.0.0.0', port=int(port), debug=False)
---
+++ i-Code-Studio/MultimodalAgent/WebApp/test.py
@@ -0,0 +1,57 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
+
+from array import array
+import os
+import sys
+import time
+
+'''
+Authenticate
+Authenticates your credentials and creates a client.
+'''
+subscription_key = "e7631c6324034caf82f704607bb25c11"
+endpoint = "https://multimodalitycvicode.cognitiveservices.azure.com"
+
+computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+'''
+END - Authenticate
+'''
+
+'''
+Quickstart variables
+These variables are shared by several examples
+'''
+# Images used for the examples: Describe an image, Categorize an image, Tag an image, 
+# Detect faces, Detect adult or racy content, Detect the color scheme, 
+# Detect domain-specific content, Detect image types, Detect objects
+#images_folder = os.path.join (os.path.dirname(os.path.abspath(__file__)), "images")
+#remote_image_url = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/landmark.jpg"
+remote_image_url = "~/logo.png"
+'''
+END - Quickstart variables
+'''
+
+
+'''
+Tag an Image - remote
+This example returns a tag (key word) for each thing in the image.
+'''
+print("===== Tag an image - remote =====")
+# Call API with remote image
+tags_result_remote = computervision_client.tag_image(remote_image_url )
+
+# Print results with confidence score
+print("Tags in the remote image: ")
+if (len(tags_result_remote.tags) == 0):
+    print("No tags detected.")
+else:
+    for tag in tags_result_remote.tags:
+        print("'{}' with confidence {:.2f}%".format(tag.name, tag.confidence * 100))
+print()
+'''
+END - Tag an Image - remote
+'''
+print("End of Computer Vision quickstart.")
